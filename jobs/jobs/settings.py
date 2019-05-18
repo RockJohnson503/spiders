cafdd12f5184c86d3669a7c7746cd4f192665004
RockJohnson503 <836867547@qq.com>
2019-05-18 19:39:40 +0800

    配置下载延迟及user_agent

--- jobs/jobs/settings.py
+++ jobs/jobs/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'jobs.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'jobs (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
@@ -29,7 +29,7 @@ DUPEFILTER_CLASS = 'jobs.dupefilters.URLBloomFilter'
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 0.5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
