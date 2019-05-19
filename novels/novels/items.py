6209dbccf03c7a485fad95f6fd856034a62a0fc5
RockJohnson503 <836867547@qq.com>
2019-05-19 14:17:30 +0800

    定义item字段

--- novels/novels/items.py
+++ novels/novels/items.py
@@ -1,14 +1,29 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class NovelsItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class YunqiBookListItem(scrapy.Item):
+    novel_id = scrapy.Field()
+    novel_name = scrapy.Field()
+    novel_link = scrapy.Field()
+    novel_author = scrapy.Field()
+    novel_type = scrapy.Field()
+    novel_status = scrapy.Field()
+    novel_update_time = scrapy.Field()
+    novel_words = scrapy.Field()
+    novel_image_url = scrapy.Field()
+
+
+class YunqiBookDetailItem(scrapy.Item):
+    novel_id = scrapy.Field()
+    novel_label = scrapy.Field()
+    novel_all_click = scrapy.Field()
+    novel_month_click = scrapy.Field()
+    novel_week_click = scrapy.Field()
+    novel_all_popular = scrapy.Field()
+    novel_month_popular = scrapy.Field()
+    novel_week_popular = scrapy.Field()
+    novel_comment_num = scrapy.Field()
+    novel_all_comm = scrapy.Field()
+    novel_month_comm = scrapy.Field()
+    novel_week_comm = scrapy.Field()
