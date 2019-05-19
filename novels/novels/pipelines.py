879bc5802c7a29d0f9e9f049ce57dcf73ce3ab51
RockJohnson503 <836867547@qq.com>
2019-05-19 20:29:33 +0800

    初始化pipelines

--- novels/novels/pipelines.py
+++ novels/novels/pipelines.py
@@ -1,11 +1,32 @@
 # -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
+from novels.items import *
 
 
-class NovelsPipeline(object):
+class YunQicrawlPipeline(object):
+    def __init__(self, mongo_uri, mongo_db, replicaset):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+        self.replicaset = replicaset
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'yunqi'),
+            replicaset=crawler.settings.get('REPLICASET')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri, replicaset=self.replicaset)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        if isinstance(item, YunqiBookListItem):
+            self.db.book_info.insert(dict(item))
+        else:
+            self.db.book_detail.insert(dict(item))
         return item
